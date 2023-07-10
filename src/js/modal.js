b26f2448104c777f45cf9a74a72ee1d2a8275a41
mr-dorian-black <mr-dorian-black@users.noreply.github.com>
2023-07-10 21:16:07 +0300

    v1.29

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtnHero: document.querySelector(".hero-modal-open"),
+    openModalBtnVegetables: document.querySelector(".vegetables-modal-open"),
+    closeModalBtn: document.querySelector(".data-modal-close"),
+    modal: document.querySelector(".data-modal"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnHero.addEventListener("click", toggleModal);
+  refs.openModalBtnVegetables.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
