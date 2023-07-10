2e63f1471b7be7d898f0995cb2e1162ecddc62c8
mr-dorian-black <mr-dorian-black@users.noreply.github.com>
2023-07-10 23:38:25 +0300

    v1.32

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,8 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu-btn');
+  const closeMenuItems = document.querySelectorAll('.js-close-menu');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -14,6 +15,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuItems.forEach(r => r.addEventListener('click', toggleMenu));
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,11 @@
 (() => {
   const refs = {
-    openModalBtnHero: document.querySelector(".hero-modal-open"),
-    openModalBtnVegetables: document.querySelector(".vegetables-modal-open"),
+    openModalBtn: document.querySelectorAll(".data-modal-open"),
     closeModalBtn: document.querySelector(".data-modal-close"),
     modal: document.querySelector(".data-modal"),
   };
 
-  refs.openModalBtnHero.addEventListener("click", toggleModal);
-  refs.openModalBtnVegetables.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach(r => r.addEventListener("click", toggleModal));
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
